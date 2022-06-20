bbfef5fb12afe13648e8a3df1e54ea33fceb0370
shaneapowell <shaneapowell@hotmail.com>
2022-06-20 17:17:31 -0500

    Fixed adding values to a nested path that is not yet fully set.

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 import sys
 import argparse
 import json
+"""
+A script to read or write a json value.
+I'm 99% sure this would be easier in nodejs.
+But, since I'm not much of a JS coder, I wrote it in the quickest language I knew.
+"""
 
 
-def _readJson(filename: str, field: str) -> str:
+def _readJson(filename: str, fields) -> str:
     """
     read the requested json field and return it
     """
     obj = json.load(open(filename))
-    print(f"GET {field}")
-    cmd = f"obj{field}" 
+    path = _listToEvalPath(fields)
+    print(f"GET {fields} == {path}")
+    cmd = f"obj{path}" 
     value = eval(cmd)
     return value
 
 
-def _writeJson(filename: str, field: str, value: str):
+def _writeJson(filename: str, fields, value: str):
     """
     write the requested field to the json file.
     """
@@ -23,8 +29,11 @@ def _writeJson(filename: str, field: str, value: str):
     obj = json.load(file)
     file.close()
 
-    print(f"SET {field} = {value}")
-    cmd = f"obj{field} = value" 
+    _validateDict(obj, fields)
+    
+    path = _listToEvalPath(fields)
+    cmd = f"obj{path} = value" 
+    print(f"SET {cmd}")
     exec(cmd)
 
     file = open(filename, "w")
@@ -32,18 +41,35 @@ def _writeJson(filename: str, field: str, value: str):
     file.flush()
     file.close()
     
+def _validateDict(obj, fields):
+    """
+    simply recursively ensure each field is in the obj
+    """
+    if len(fields) > 1:
+        field = fields[0]
+        if field not in obj or not isinstance(obj[field], dict):
+            obj[field] = {}
+        return _validateDict(obj[field], fields[1:])
+    else:
+        return obj
 
-def _fieldToDictList(field: str):
+def _fieldToList(field: str):
     """
     Convert a dot-notation sequence, into a named dict reference.
     eg.  "user.address.zip" == ['user']['address']['zip']
+    Doesn't yet handle arrays
     """
-    dictPath = ""
-    fieldList = field.split('.')
-    for f in fieldList:
-        dictPath += f"['{f}']"
-    return dictPath
+    return field.split('.')
 
+def _listToEvalPath(fields):
+    """
+    Convert a list to the dict eval string
+    ['root', 'name', 'first'] --> ['root']['name']['first']
+    """
+    path = ""
+    for f in fields:
+        path += f"['{f}']"
+    return path
 
 
 def main():
@@ -57,15 +83,14 @@ def main():
     
     args = parser.parse_args()
 
-    field = _fieldToDictList(args.field)
+    fields = _fieldToList(args.field)
 
     if args.action == 'write':
-        _writeJson(args.filename, field, args.value)
+        _writeJson(args.filename, fields, args.value)
     else:
-        val = _readJson(args.filename, field)
+        val = _readJson(args.filename, fields)
         print(f"::set-output name=value::{val}")
 
 if __name__ == '__main__':
-    print(sys.argv[1:])
     main()
 
